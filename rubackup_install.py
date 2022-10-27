82ec8a722ecae5bac6a20f398853cee125ff2229
Arkadiy Nechaev <anechaev@rubackup.ru>
2022-10-27 20:29:51 +0300

    changes in install script

--- rubackup_install.py
+++ rubackup_install.py
@@ -7,7 +7,6 @@ import subprocess
 
 additional_packages = ["pigz","mailutils","xz-utils","nfs-common","libcurl4","nfs-kernel-server","postgresql","libqt5sql5-psql","qt5-default"]
 
-#subprocess.Popen(["sudo","apt","update"],stdin=subprocess.PIPE, stdout=subproces, stderr=subprocess.PIPE).communicate(input=b'31\n')
 subprocess.Popen(["sudo","apt","update"],stdin=subprocess.PIPE).communicate(input=b'31\n')
 
 for addpack_name in additional_packages:
