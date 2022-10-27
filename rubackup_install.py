ba80908f1a18f40f667269f12bab6b52dafc157a
Arkadiy Nechaev <anechaev@rubackup.ru>
2022-10-27 11:44:59 +0300

    changes in install script

--- rubackup_install.py
+++ rubackup_install.py
@@ -1,10 +1,15 @@
 import wget
+import os
 
+packages_path = '/home/u/rubackup-latest/'
 packages = ["rubackup-server.deb", "rubackup-client.deb", "rubackup-common.deb", "rubackup-rbm.deb"]
 
 for package_name in packages:
-    print(f'Downloading {package_name}...')
+    if os.path.isfile(packages_path):
+        print(f'Removing {package_name}...')
+        os.remove(packages_path + package_name)
+    else:
+        print(f'Downloading {package_name}...')
 
-    url = f'http://10.177.32.37:8080/latest/deb/ubuntu/{package_name}'
-    wget.download(url, f'/home/u/rubackup-latest/{package_name}')
-
+        url = f'http://10.177.32.37:8080/latest/deb/ubuntu/{package_name}'
+        wget.download(url, f'{packages_path}{package_name}')
